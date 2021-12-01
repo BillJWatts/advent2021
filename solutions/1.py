8d013967be84123fa56b3d8a0e60aedba7bdec88
Bill Watts <bill.j.watts@gmail.com>
2021-12-01 22:37:51 +0000

    resources config

---
+++ solutions/1.py
@@ -0,0 +1,20 @@
+"""Solution to day 1 problem"""
+
+from config import RESOURCES
+
+with open(RESOURCES + "1.txt", "r") as f:
+    values = [int(value) for value in f.readlines()]
+
+total_values = len(values)
+
+increases = 0
+for i, value in enumerate(values):
+    if i > 0 and i < total_values - 2:
+        A = sum([values[i - 1], value, values[i + 1]])
+        B = sum([value, values[i + 1], values[i + 2]])
+
+        if A < B:
+            increases += 1
+
+
+print(increases)
