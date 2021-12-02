d4f03fc36413651470565fda37981b25efc0f2c6
Bill Watts <bill.j.watts@gmail.com>
2021-12-02 17:09:40 +0000

    Day 2

---
+++ solutions/2.py
@@ -0,0 +1,46 @@
+from config import RESOURCES
+
+with open(RESOURCES + "2.txt") as f:
+    commands = [tuple(com.split()) for com in f.readlines()]
+
+
+""" --- PART ONE --- """
+horizontal_pos: int = 0
+vertical_pos: int = 0
+
+for direction, magnitude in commands:
+    if direction == "forward":
+        horizontal_pos += int(magnitude)
+
+    elif direction == "up":
+        vertical_pos -= int(magnitude)
+
+    elif direction == "down":
+        vertical_pos += int(magnitude)
+
+print(" --- PART ONE ---")
+print(f"Horizontal pos: {horizontal_pos}")
+print(f"Vertical pos: {vertical_pos}")
+print(f"Multiplied: {horizontal_pos * vertical_pos}")
+
+
+""" --- PART TWO --- """
+horizontal_pos: int = 0
+vertical_pos: int = 0
+aim: int = 0
+
+for direction, magnitude in commands:
+    if direction == "forward":
+        horizontal_pos += int(magnitude)
+        vertical_pos += int(magnitude) * aim
+
+    elif direction == "up":
+        aim -= int(magnitude)
+
+    elif direction == "down":
+        aim += int(magnitude)
+
+print(" --- PART TWO ---")
+print(f"Horizontal pos: {horizontal_pos}")
+print(f"Vertical pos: {vertical_pos}")
+print(f"Multiplied: {horizontal_pos * vertical_pos}")
